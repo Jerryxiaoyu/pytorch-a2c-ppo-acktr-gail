0f63a47ff807c26a5b4034142ab0f7a0d146b937
Jerryxiaoyu <wjywangjiay@163.com>
2019-09-20 10:01:01 +0800

    add requirments

--- test/test_remote_files.py
+++ test/test_remote_files.py
@@ -21,7 +21,7 @@ group_dir = os.path.join(root_path, 'logs-files', exp_group_dir)
 
 # SSH Config
 if remote_FLAG:
-    hostname = '101.6.57.83'
+    hostname = "fe80::6ca2:7e30:9ca3:a287"
     username = 'drl'
     key_path = '/home/ubuntu/.ssh/id_rsa'
 
