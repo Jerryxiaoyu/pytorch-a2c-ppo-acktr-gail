8690fb7f9ca777af78d2ab53fb1d0fa5e4406450
Jerryxiaoyu <wjywangjiay@163.com>
2020-07-06 13:43:32 +0800

    add eval version1.0

--- run20-cell6-ppo.py
+++ run20-cell6-ppo.py
@@ -10,7 +10,7 @@ class VG(VariantGenerator):
 
     @variant
     def env_name(self):
-        return ['CellRobotEnvCPG6Goal-v1' ]  # 'CellrobotEnv-v0' , 'Cellrobot2Env-v0', 'CellrobotSnakeEnv-v0'  , 'CellrobotSnake2Env-v0','CellrobotButterflyEnv-v0', 'CellrobotBigdog2Env-v0'
+        return ['CellRobotEnvCPG6Goal-v2' ]  # 'CellrobotEnv-v0' , 'Cellrobot2Env-v0', 'CellrobotSnakeEnv-v0'  , 'CellrobotSnake2Env-v0','CellrobotButterflyEnv-v0', 'CellrobotBigdog2Env-v0'
 
     @variant
     def seed(self):
@@ -95,7 +95,7 @@ class VG(VariantGenerator):
 
     @variant
     def action_dim(self):
-        return [39 ]  # 2,3,13
+        return [13 ]  # 2,3,13
 
     @variant
     def CPG_enable(self):
@@ -119,7 +119,7 @@ class VG(VariantGenerator):
         return [0]
 
 
-exp_id = 15
+exp_id = 16
 EXP_NAME ='_SMC_PPO_RL_CELL6'
 group_note ="************ABOUT THIS EXPERIMENT****************\n" \
             "  " \
